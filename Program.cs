9221169b69dae420c585e1cfbeb22b9dcc3dc9ab
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-19 21:56:20 -0300

    Estava tentando de varias formas, essa foi a ultima

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Elefante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int qnt;
+            int cont1 = 0;
+            int cont2;
+            
+            string flexao;
+
+            Console.Write("Quantos elefantes: ");
+            qnt = Convert.ToInt32(Console.ReadLine());
+
+            cont2 = qnt;
+
+            while (cont2 > 1)
+            {
+                cont1++;
+                
+                
+
+                if(cont1 > 1){
+
+                flexao = "elefantes";
+                }else{
+                    flexao = "elefante";
+                }
+
+                Console.Write($"{cont1} {flexao} incomoda muita gente\n{cont1 + 1} {flexao} ");
+
+                while (qnt > 1)
+                {
+                    qnt--;
+                    qnt--;
+                     Console.Write("incomodam ");
+                }
+                        
+                qnt = cont1;
+                Console.WriteLine("muito mais");
+                cont2--;
+                
+            }
+
+            
+
+            
+        }
+    }
+}
